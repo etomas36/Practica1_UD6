d535956e09b9eb47bae22c1f8d223bd02c5b70f7
Camilo Juan <c.juancasil@edu.gva.es>
2023-03-11 21:04:30 +0100

    Create Persona.java

---
+++ src/main/java/ed/camilo/Persona.java
@@ -0,0 +1,22 @@
+package ed.camilo;
+
+public class Persona {
+	
+	
+	public String n;
+	public String c;
+	public int a;
+	public String cl;
+	
+	@Override
+	public String toString() {
+		
+		return  "n = " + n + System.lineSeparator() +
+				"c = " + c + System.lineSeparator() +
+				"a = " + a + System.lineSeparator() +
+				"cl = " + cl;
+				
+		
+	}
+
+}
