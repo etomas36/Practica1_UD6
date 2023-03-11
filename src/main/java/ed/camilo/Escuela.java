4259bb3c41622040a59f25e81a305b0561b17b45
Camilo Juan <c.juancasil@edu.gva.es>
2023-03-11 21:05:19 +0100

    Create Escuela.java

---
+++ src/main/java/ed/camilo/Escuela.java
@@ -0,0 +1,18 @@
+package ed.camilo;
+
+public class Escuela {
+
+	public static void main(String[] args) {
+		
+		Persona p = new Persona();
+		
+		p.n = "Camilo";
+		p.c = "Juan";
+		p.cl = "1º DAM/DAW";
+		p.a = 2023;
+		
+		System.out.println(p.toString());
+
+	}
+
+}
